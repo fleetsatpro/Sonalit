0132c1edc1dd63be75e55bf3ffb8cd7e000fbc45
FleetOps <admin@fleetops.local>
2026-05-11 09:35:44 +0000

    Fix main.jsx: clean version without ErrorBoundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary><App /></ErrorBoundary>
+    <App />
     <Toaster
       position="top-right"
       toastOptions={{
