647125d4e3ec3ef0372b44ecc82d3d9e92e11d6d
fleetsatpro <griphilgaming@gmail.com>
2026-09-05 07:57:47 +0300

    feat(email): run Resend worker with notification worker

--- backend/src/workers/worker.notification.js
+++ backend/src/workers/worker.notification.js
@@ -3,13 +3,18 @@ const logger = require('../utils/logger');
 const { pool } = require('../config/database');
 const { createQueues } = require('../config/queue');
 const { startNotificationWorker } = require('./notificationWorker');
+const { startResendEmailWorker } = require('./resendEmailWorker');
 
 createQueues();
-const worker = startNotificationWorker();
+const fanoutWorker = startNotificationWorker();
+const resendWorker = startResendEmailWorker();
 
 async function shutdown() {
-  logger.info('Notification worker shutting down');
-  await worker.close();
+  logger.info('Notification/email workers shutting down');
+  await Promise.all([
+    fanoutWorker.close().catch(() => {}),
+    resendWorker.close().catch(() => {}),
+  ]);
   await pool.end().catch(() => {});
   process.exit(0);
 }
