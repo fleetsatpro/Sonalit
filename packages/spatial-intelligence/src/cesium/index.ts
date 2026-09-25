ee91f49477c9aae5aab7362715028e7cda110212
fleetsatpro <griphilgaming@gmail.com>
2026-09-26 00:32:13 +0300

    feat(spatial): export satellite LOD API

--- packages/spatial-intelligence/src/cesium/index.ts
+++ packages/spatial-intelligence/src/cesium/index.ts
@@ -7,3 +7,19 @@ export type {
   SpatialEntityDescriptor,
   SelectionContext,
 } from './EntityRegistry.js';
+
+export { 
+  classifySatelliteLod,
+  selectSatellitesForLod,
+  satelliteGeoJson,
+  DEFAULT_SATELLITE_LOD_BUDGET,
+  SATELLITE_LOD_HEIGHTS_M,
+  SATELLITE_MODELLED_CAPTION,
+} from './SatelliteLod.js';
+export type {
+  SatelliteGeoJsonPointFeature,
+  SatelliteLodBudget,
+  SatelliteLodOptions,
+  SatelliteLodItem,
+  SatelliteLodTier,
+} from './SatelliteLod.js';
